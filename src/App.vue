cb2c5f5bf93a7a4c42b6468cdd42976a66b82312
MarybethGasman <2934482133@qq.com>
2023-04-25 08:26:44 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref, watch } from 'vue';
+import { reactive, ref, watch, watchEffect } from 'vue';
 import { functionUnitStates, readOperand, execute, issue, writeback } from './vm/machine.js';
 
 let cycle = 0
@@ -17,7 +17,7 @@ ADD R6 R8 R2
 let instructions = code.value.split('\n').filter(s => s.length !== 0)
 
 watch(code, (newV, oldV) => {
-instructions = newV.split('\n').filter(s => s.length !== 0)  
+  instructions = newV.split('\n').filter(s => s.length !== 0)
 })
 
 const prevCycle = () => {
@@ -32,16 +32,18 @@ time.set('DIV', 40)
 time.set('SUB', 2)
 
 let next = 0
-let roJobs = []
-let executeJobs = []
-let wbJobs = []
+
+let roJobs = reactive([])
+let executeJobs = reactive([])
+let wbJobs = reactive([])
+
 let futureCycleJobs = []
 const doFutureCycleJobs = () => {
   futureCycleJobs
-  .filter(f => f.remain === 1)
-  .forEach(future => {
-    future.queue.push(future.job)
-  })
+    .filter(f => f.remain === 1)
+    .forEach(future => {
+      future.queue.push(future.job)
+    })
 
   futureCycleJobs = futureCycleJobs.filter(f => f.remain > 1)
   futureCycleJobs = futureCycleJobs.map(f => {
@@ -52,7 +54,9 @@ const doFutureCycleJobs = () => {
 const nextCycle = () => {
   cycle++
   doFutureCycleJobs()
-  if(issue(instructions[next]) !== undefined) {
+  if (issue(instructions[next]) !== undefined) {
+    console.log(cycle, " 发射 ", instructions[next]);
+    update(instructions[next], cycle)
     futureCycleJobs.push({
       queue: roJobs,
       remain: 1,
@@ -60,31 +64,68 @@ const nextCycle = () => {
     })
     next++
   }
-  let job = readOperand(roJobs[0])
-  if(job !== undefined) {
-    roJobs.shift()
+  let over = readOperand(roJobs)
+
+  over.forEach(job => {
+    console.log(cycle, " 读操作数 ", job);
+    update(job, cycle)
+    let index = roJobs.indexOf(job);
+    if (index !== -1) {
+      roJobs.splice(index, 1);
+    }
     futureCycleJobs.push({
       queue: executeJobs,
       remain: 1,
       job: job,
     })
-  }
-  job = execute(executeJobs[0])
-  if(job !== undefined) {
-    executeJobs.shift()
+  })
+
+  over = execute(executeJobs)
+  over.forEach(job => {
+    console.log(cycle, " 执行 ", job);
+    update(job, cycle)
+
+    let index = executeJobs.indexOf(job);
+    if (index !== -1) {
+      executeJobs.splice(index, 1);
+    }
     futureCycleJobs.push({
       queue: wbJobs,
       remain: time.get(job.split(' ')[0]),
       job: job,
     })
-  }
-  job = writeback(wbJobs[0])
-  if(job !== undefined) {
-    wbJobs.shift()
-  }
-  console.log(cycle, functionUnitStates);
+  })
+
+  over = writeback(wbJobs)
+  over.forEach(job => {
+    console.log(cycle, " 写回 ", job);
+    update(job, cycle)
+    let index = wbJobs.indexOf(job);
+    if (index !== -1) {
+      wbJobs.splice(index, 1);
+    }
+  })
+  // console.log(cycle, functionUnitStates);
+  fullState.push(JSON.parse(JSON.stringify(state)))
+  // if(cycleView.value === fullState.length - 1) {
+  //   fullState.push(fullState[fullState.length - 1])
+  // }
+
   cycleView.value++
 }
+
+const update = (instruction, cycle) => {
+  if(state[instruction] === undefined) {
+    state[instruction] = [cycle]
+  }else {
+    state[instruction].push(cycle)
+  }
+}
+
+let fullState = [{}]
+
+let state = {}
+
 </script>
 
 <template>
@@ -92,12 +133,12 @@ const nextCycle = () => {
     <div class="text-center text-xl">当前是第 {{ cycleView }} 个周期 <button @click="nextCycle">下一步</button> <button
         @click="prevCycle">上一步</button> </div>
 
-    <!-- <textarea v-model="code" name="" id="" cols="30" rows="10"></textarea> -->
-    <!-- <table class="border-collapse table-auto w-full text-sm">
+    <textarea v-model="code" name="" id="" cols="30" rows="10"></textarea>
+    <table class="border-collapse table-auto w-full text-sm">
       <thead>
         <tr>
           <th class="border-b  font-semibold p-4 pl-8 pt-0 pb-3 text-left">指令</th>
-          <th class="border-b  font-semibold p-4 pt-0 pb-3 text-left">流出</th>
+          <th class="border-b  font-semibold p-4 pt-0 pb-3 text-left">发射</th>
           <th class="border-b  font-semibold p-4 pr-8 pt-0 pb-3 text-left">读数</th>
           <th class="border-b  font-semibold p-4 pr-8 pt-0 pb-3 text-left">执行</th>
           <th class="border-b  font-semibold p-4 pr-8 pt-0 pb-3 text-left">写回</th>
@@ -107,11 +148,11 @@ const nextCycle = () => {
         <tr v-for="i, index in instructions">
           <td class="border-b border-slate-100 dark:border-slate-700 p-2 pl-8 text-slate-500 dark:text-slate-400">{{ i }}
           </td>
-          <td v-for="sv, j in fullCycleView[cycleView][index]"
+          <td v-for="sv in fullState[cycleView][i]"
             class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">{{ sv }}</td>
         </tr>
       </tbody>
-    </table> -->
+    </table>
 
     <!-- <ul class="text-xl">
       <li v-for="f in fullFuctionStateView[cycleView].filter(f => f !== undefined)">
--- src/vm/machine.js
+++ src/vm/machine.js
@@ -43,13 +43,19 @@ export let istatesfinished = new Array(10).fill(0)
 // 1 加法和减法单元
 // 2 乘法单元
 // 3 除法单元
-export let functionUnitStates = {}
+export let functionUnitStates = []
 
 let result = new Array(15)
 
 // 下一个将要issue的指令
 let next = 0;
 
+let units = new Map()
+
+units.set("ADD", 1)
+units.set("LD", 1)
+units.set("MUL", 1)
+units.set("DIV", 1)
 
 
 // 一些未来周期的修改istatesfinished的任务
@@ -66,91 +72,101 @@ export function issue(instruction) {
     src1 = +src1?.slice(1)
     src2 = +src2?.slice(1)
     dst = +dst?.slice(1)
-    if ((result[dst]?.done ?? true) === false
-        || (functionUnitStates[op]?.done ?? true) === false) {
+    if (result[dst] !== undefined
+        || units.get(op) === 0) {
         return undefined
     }
 
-    console.log("issue", instruction);
-    functionUnitStates[op] = {
-        busy: true,
+    units.set(op, units.get(op) - 1)
+    // console.log("issue", instruction);
+    let f = {
         op: op,
         src1: src1,
         src2: src2,
         q1: result[src1],
         q2: result[src2],
-        r1: result[src1] === undefined,
-        r2: result[src2] === undefined,
+        // r1: result[src1] === undefined,
+        // r2: result[src2] === undefined,
         dst: dst,
+        readDone: false,
         done: false
     }
+    functionUnitStates.push(f)
 
-    result[dst] = functionUnitStates[op]
+    result[dst] = f
     return instruction
 }
 
-export function readOperand(instruction) {
+export function readOperand(roJobs) {
+    let over = []
 
-    if (instruction === undefined) {
-        return undefined
-    }
+    roJobs.forEach(instruction => {
+        let [op, dst, src1, src2] = instruction.split(' ')
+        src1 = +src1?.slice(1)
+        src2 = +src2?.slice(1)
+        dst = +dst?.slice(1)
+        for (let i = 0; i < functionUnitStates.length; i++) {
+            let f = functionUnitStates[i]
+            if (f === result[dst]) {
+                break
+            }
+            if ((f.dst === src1 || f.dst === src2)
+                && f.done === false) {
+                return undefined
+            }
+        }
 
-    let [op, dst, src1, src2] = instruction.split(' ')
-    if ((functionUnitStates[op]?.q1?.done ?? true) === false
-        || (functionUnitStates[op]?.q2?.done ?? true) === false) {
-        return undefined
-    }
-    src1 = +src1?.slice(1)
-    src2 = +src2?.slice(1)
-    dst = +dst?.slice(1)
-
-
-    console.log("读操作数", instruction);
-
-    return instruction
+        over.push(instruction)
+        result[dst].q1 = undefined
+        result[dst].q2 = undefined
+        // console.log("读操作数", instruction);
+    })
+    return over
 }
 
 
-export function execute(instruction) {
+export function execute(executeJobs) {
+    let over = []
 
-    if (instruction === undefined) {
-        return undefined
-    }
+    executeJobs.forEach(instruction => {
+        let [op, dst, src1, src2] = instruction.split(' ')
+        src1 = +src1?.slice(1)
+        src2 = +src2?.slice(1)
+        dst = +dst?.slice(1)
 
-    let [op, dst, src1, src2] = instruction.split(' ')
-    src1 = +src1?.slice(1)
-    src2 = +src2?.slice(1)
-    dst = +dst?.slice(1)
-    console.log("执行", instruction);
-    return instruction
-}
+        result[dst].readDone = true
 
-export function writeback(instruction) {
-
-    if (instruction === undefined) {
-        return undefined
-    }
-    let [op, dst, src1, src2] = instruction.split(' ')
-    src1 = +src1?.slice(1)
-    src2 = +src2?.slice(1)
-    dst = +dst?.slice(1)
-    console.log("写回", instruction);
-
-    result[dst].done = true
-    functionUnitStates[op] = undefined
-    return instruction
-}
-
-export function doPendingJobs() {
-    iStateFinishedPendingJob.filter(j => j.remain === 1).forEach(j => {
-        istatesfinished[j.iindex] = j.istate
+        // console.log("执行", instruction);
+        over.push(instruction)
     })
 
-    iStateFinishedPendingJob = iStateFinishedPendingJob.filter(j => j.remain > 1).map(j => {
-        j.remain--
-        return j
-    })
+    return over
 }
-export function print(clock) {
-    console.log(clock, istates, functionUnitStates);
+
+export function writeback(wbJobs) {
+    let over = []
+    wbJobs.forEach(instruction => {
+        let [op, dst, src1, src2] = instruction.split(' ')
+        op = op === "SUB" ? "ADD" : op
+        src1 = +src1?.slice(1)
+        src2 = +src2?.slice(1)
+        dst = +dst?.slice(1)
+
+        for (let i = 0; i < functionUnitStates.length; i++) {
+            let f = functionUnitStates[i]
+            if (f === result[dst]) {
+                break
+            }
+            if ((f.src1 === dst || f.src2 === dst)
+                && f.readDone === false) {
+                return undefined
+            }
+        }
+        // console.log("写回", instruction, functionUnitStates);
+        over.push(instruction)
+        units.set(op, units.get(op) + 1)
+        result[dst].done = true
+        result[dst] = undefined
+    })
+    return over
 }
